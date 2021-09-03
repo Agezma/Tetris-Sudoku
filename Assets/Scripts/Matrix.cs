ce1bf21ff20260124658c3a18a6430170e9eb99d
Agezma <Agezma@live.com>
2021-09-03 17:39:11 -0300

    Primeras cosas

---
+++ Assets/Scripts/Matrix.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+
+public class Matrix<T> : IEnumerable<T>
+{
+    T[] matrix;
+
+    public int Width { get; private set; }
+
+    public int Height { get; private set; }
+
+    public int Capacity { get; private set; }
+
+    public void Initialize( int width, int height)
+    {
+        Width = width;
+        Height = height;
+        Capacity = width * height;
+        matrix = new T[Capacity];
+    }
+
+    public Matrix(int width, int height)
+    {
+        Initialize(width, height);
+    }
+
+	public Matrix(T[,] copyFrom)
+    {
+        Initialize(copyFrom.GetLength(0), copyFrom.GetLength(1));
+
+        for (int i = 0; i < Capacity; i++)
+        {
+            matrix[i] = copyFrom[i % Width, i / Width];
+        }
+    }
+
+	public Matrix<T> Clone() {
+        Matrix<T> aux = new Matrix<T>(Width, Height);      
+      
+        for (int i = 0; i < Width; i++)
+        {
+            for (int j = 0; j < Height; j++)
+            {
+                aux[i, j] = this[i, j];
+            }
+
+        }
+        return aux;
+    }
+
+	public void SetRangeTo(int x0, int y0, int x1, int y1, T item) 
+    {
+        int initPos = x0 + y0 * Width;
+        int endPos = x1 + y1 * Width;
+
+        for (int i = initPos; i < endPos; i++)
+        {
+            matrix[i] = item;
+        }
+    }
+
+    public List<T> GetRange(int x0, int y0, int x1, int y1) {
+        List<T> aux = new List<T>();
+
+        int initPos = x0 + y0 * Width;
+        int endPos = x1 + y1 * Width;
+
+        for (int i = x0; i < x1; i++)
+        {
+            for (int j = y0; j < y1; j++)
+            {
+                aux.Add( matrix[i + j * Width]);
+            }
+        }
+        return aux;
+	}
+
+    public T this[int x, int y] {
+		get
+        {
+            var index = x + y * Width;     
+            return matrix[index];
+		}
+		set 
+        {
+            var index = x + y * Width;
+            matrix[index] = value;
+        }
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        for (int i = 0; i < Capacity; i++)
+        {
+            yield return matrix[i];
+        }
+    }
+
+	IEnumerator IEnumerable.GetEnumerator() {
+		return GetEnumerator();
+	}
+}
---
+++ Assets/Scripts/Sudoku.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sudoku : MonoBehaviour
+{
+    public Cell prefabCell;
+    public Canvas canvas;
+
+    Matrix<Cell> _board;
+
+    List<int> nums = new List<int>();
+
+
+    Matrix<int> _createdMatrix;
+
+    int _smallSideX;
+    int _smallSideY;
+    int _bigSideX;
+    int _bigSideY;
+
+
+    private void Start()
+    {
+        CreateEmptyBoard();
+        ClearBoard();
+
+        CreateSudoku();
+    }
+
+    
+
+    void CreateEmptyBoard()
+    {
+        float spacing = 68f;
+        float startX = -spacing * 4f;
+        float startY = spacing * 4f;
+
+        _board = new Matrix<Cell>(_bigSideX, _bigSideY);
+        for (int x = 0; x < _board.Width; x++)
+        {
+            for (int y = 0; y < _board.Height; y++)
+            {
+                var cell = _board[x, y] = Instantiate(prefabCell);
+                cell.transform.SetParent(canvas.transform, false);
+                cell.transform.localPosition = new Vector3(startX + x * spacing, startY - y * spacing, 0);
+            }
+        }
+    }
+
+    void CreateSudoku()
+    {
+        StopAllCoroutines();
+        nums = new List<int>();
+        ClearBoard();
+        List<Matrix<int>> l = new List<Matrix<int>>();     
+
+        TranslateAllValues(_createdMatrix);
+  
+    }
+
+    void TranslateAllValues(Matrix<int> matrix)
+    {
+        for (int y = 0; y < _board.Height; y++)
+        {
+            for (int x = 0; x < _board.Width; x++)
+            {
+                _board[x, y].number = matrix[x, y];
+            }
+        }
+    }
+
+    void ClearBoard()
+    {
+        _createdMatrix = new Matrix<int>(_bigSideX, _bigSideY);
+        foreach (var cell in _board)
+        {
+            cell.number = 0;
+            cell.locked = cell.invalid = false;
+        }
+    }
+}
