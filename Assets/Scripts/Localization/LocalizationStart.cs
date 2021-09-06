54f80501fdd8e84f7f069c0c262de386df6bfa1b
Agezma <Agezma@live.com>
2021-09-06 17:12:38 -0300

    Localization

---
+++ Assets/Scripts/Localization/LocalizationStart.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocalizationStart : MonoBehaviour {
+
+    void Awake()
+    {
+        if (Localization.Instance.currentLanguage == null)
+            Localization.Instance.LoadFromJson("Assets/Languages/es_AR.json");
+    }
+}
