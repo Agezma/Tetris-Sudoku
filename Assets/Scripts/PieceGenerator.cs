ebeccaaffe289730035a0e9e66a2e0eadb3d0492
Belen Nava <32393295+Belu123mm@users.noreply.github.com>
2021-09-07 00:26:35 -0300

    Pieza Generador

---
+++ Assets/Scripts/PieceGenerator.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PieceGenerator : MonoBehaviour
+{
+    public List<Piece> pieces;
+    public Piece GeneratePiece(){
+        return Instantiate(pieces[Random.Range(0,pieces.Count)] as Piece);
+    }
+
+}
